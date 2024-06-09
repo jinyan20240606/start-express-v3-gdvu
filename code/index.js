e228257f983b8d456c1db8f160afb139f0780195
serverless-devs <serverless-devs@alibaba-inc.com>
2024-06-09 10:12:46 +0000

    "333333"

--- code/index.js
+++ code/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const feishuServer = require("./feishu-server");
 
 const app = express();
 
@@ -8,13 +9,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.raw());
 
-app.all("/*", (req, res) => {
+app.all("/*", async(req, res) => {
   console.log(req.body, '12--------')
+  const res1 = await feishuServer(req.body);
+  console.log(res1, '12-----2-------')
   const requestId = req.headers["x-fc-request-id"];
   console.log("FC Invoke Start RequestId: " + requestId);
 
   res.send(
-    JSON.stringify(req.body)
+    JSON.stringify(res1)
   );
 
   console.log("FC Invoke End RequestId: " + requestId);
